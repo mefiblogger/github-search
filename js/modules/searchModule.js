ca669344ae72cc66db38eefc688a7e977c5bc7f5
Nádai Gábor <nadai.gabor@ingatlan.com>
2016-04-17 02:40:21 +0200

    Searching for repositories
    - search through GitHub API
    - render results
    - error messages
    - new class in CSS for centering

--- js/modules/searchModule.js
+++ js/modules/searchModule.js
@@ -1,5 +1,33 @@
 var searchModule = angular.module('search', ['ng']);
 
-searchModule.controller('SearchCtrl', function ($scope, $routeParams) {
+searchModule.controller('SearchCtrl', function ($scope, $routeParams, $http) {
+    $scope.error = false;
+    $scope.term = decodeURI($routeParams.repo);
+    $scope.searchInProgress = true;
+    $scope.found = 0;
+    $scope.repositories = [];
 
+    /**
+     * Search through GitHub API.
+     *
+     * @param term
+     */
+    $scope.searchRepositoriesByTerm = function(term) {
+        $scope.searchInProgress = true;
+        $scope.error = false;
+
+        $http({
+            method: 'GET',
+            url: 'https://api.github.com/search/repositories?q=' + encodeURI(term),
+        }).then(function successCallback(response) {
+            console.log(response);
+            $scope.found = response.data.total_count;
+            $scope.repositories = response.data.items;
+            $scope.searchInProgress = false;
+        }, function errorCallback(response) {
+            $scope.error = true;
+        });
+    };
+
+    $scope.searchRepositoriesByTerm($routeParams.repo);
 });
